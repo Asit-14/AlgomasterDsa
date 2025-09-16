3c8f93ea514260683fd413960977b7fae9bcdcad
Asit-14 <asitshakya789@gmail.com>
2025-09-16 07:29:21 +0530

    Add implementation for moving zeroes to the start of an array

---
+++ Arrays/Q2_MoveZeroesStart.java
@@ -0,0 +1,50 @@
+public class Q2_MoveZeroesStart {
+    public static void main(String[] args) {
+        int num1[] = {1, 0, 2, 0, 3};
+        moveZeroesToStartapp(num1);
+        for (int i = 0; i < num1.length; i++) {
+            System.out.print(num1[i] + " ");
+        }
+        System.out.println();
+
+        int num2[] = {5, 0, 1, 0, 2};
+        moveZeroesToStart(num2);
+        for (int i = 0; i < num2.length; i++) {
+            System.out.print(num2[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void moveZeroesToStartapp(int[] num) {
+        int n = num.length;
+        int idx = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (num[i] != 0) {
+                num[idx] = num[i];
+                idx--;
+            }
+        }
+        while (idx >= 0) {
+            num[idx] = 0;
+            idx--;
+        }
+    }
+
+    public static void moveZeroesToStart(int[] num) {
+        int n = num.length;
+        int writePos = n - 1;
+        
+        // Move all non-zero elements to the end
+        for (int i = n - 1; i >= 0; i--) {
+            if (num[i] != 0) {
+                num[writePos] = num[i];
+                writePos--;
+            }
+        }
+        
+        // Fill remaining positions with zeros
+        while (writePos >= 0) {
+            num[writePos] = 0;
+            writePos--;
+        }
+    }
+}
