f7b5d65c7bb922b570f2f7d00ca08f0b3cb6f02f
Asit-14 <asitshakya789@gmail.com>
2025-09-17 20:57:06 +0530

    Add implementation for removing duplicates from an array

---
+++ Arrays/Q3_RemoveDuplicates.java
@@ -0,0 +1,26 @@
+public class Q3_RemoveDuplicates {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 2, 4, 5, 5, 6, 6 }; 
+        int res = removeDuplicates(arr);
+        System.out.println(res);
+
+        
+        for (int i = 0; i < res; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static int removeDuplicates(int[] arr) {
+        int n = arr.length;
+        if (n == 0) return 0;
+        
+        int l = 1; 
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[l - 1]) { 
+                arr[l] = arr[i];
+                l++;
+            }
+        }
+        return l;
+    }
+}
