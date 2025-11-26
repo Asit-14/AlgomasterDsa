d89c96d0c6dd227cf64be03a8446b5880b009caa
Asit-14 <asitshakya789@gmail.com>
2025-11-26 22:29:22 +0530

    Add implementation of factorial calculation for non-negative integers

---
+++ BasicsMaths/factorialOfNumber.java
@@ -0,0 +1,22 @@
+package BasicsMaths;
+
+public class factorialOfNumber {
+
+    public static void main(String[] args) {
+        int n = 6;
+        long factorial = factorial(n);
+        System.out.println("Factorial of " + n + " is " + factorial);
+    }
+
+    public static long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+        }
+
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
