a93fba084d67dadc9c52edb182c14253f868a8b0
Asit-14 <asitshakya789@gmail.com>
2025-11-26 22:29:09 +0530

    Add implementation for checking prime numbers

---
+++ BasicsMaths/checkPrimeNumber.java
@@ -0,0 +1,28 @@
+package BasicsMaths;
+
+public class checkPrimeNumber {
+
+    public static void main(String[] args) {
+        int number = 37;
+        boolean isPrime = isPrime(number);
+        System.out.println(number + (isPrime ? " is" : " is not") + " a prime number");
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number <= 3) {
+            return true;
+        }
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
