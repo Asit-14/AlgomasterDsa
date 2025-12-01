2534822fca1962154a5e8ae318170804761b8479
Hey Asit <138616791+Asit-14@users.noreply.github.com>
2025-12-01 21:52:08 +0530

    Update Q2_firstNonRepeated.java

--- maang/Q2_firstNonRepeated.java
+++ maang/Q2_firstNonRepeated.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Q2_firstNonRepeated {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter String: ");
+        System.out.print("Enter String : ");
         String str = sc.nextLine();
         System.out.println("Answer: " + noRepeat(str));
     }
