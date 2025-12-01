eba2ddaadbf82412811d8cb7eb34c4259cce296f
Hey Asit <138616791+Asit-14@users.noreply.github.com>
2025-12-01 21:52:27 +0530

    Change input string to 'swisss' for testing

--- String/Q10_FirstNonRepeated.java
+++ String/Q10_FirstNonRepeated.java
@@ -2,7 +2,7 @@ package String;
 
 public class Q10_FirstNonRepeated {
     public static void main(String[] args) {
-        String s = "swiss";
+        String s = "swisss";
         for(char ch : s.toCharArray()){
             if (s.indexOf(ch) == s.lastIndexOf(ch)) {
                 System.out.println(ch);
